73acaa452db2ab23aaea84ba3d1b6f1d7bc6774e
Olufunto Oyewole <funti009@gmail.com>
2022-05-25 19:34:18 +0100

    created the hero section of the landing page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from 'next/image';
 import styles from '../styles/Home.module.css'
+import tempBlood from '../assets/images/blood.jpg'
+
+
 
 export default function Home() {
+
   return (
-    <div className={styles.container}>
+    <section className={styles.heroSection}>
       
-    </div>
+      
+      <div className={styles.imageContainer}>
+        <Image className={styles.theImage} placeholder="blur"  src={tempBlood} alt="a picture for the movie" layout="fill"/>
+      </div>
+      
+      <section className={styles.heroCover}>
+            <div className={styles.coverTop}>
+                <h1>Blood Sisters</h1>
+                <div>
+                    <i>*****</i>
+                    <p><span>7.4</span>/10</p>
+                    <p>IMDB</p>
+                </div>
+            </div>
+
+      
+        
+      </section>
+      
+
+      
+    </section>
   )
 }
